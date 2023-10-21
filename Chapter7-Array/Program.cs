aabb4d4679419c0cb9c21a1a0b2736cd42eeb9c0
Adekoya Yusuf <143264401+DevYusuf01@users.noreply.github.com>
2023-10-21 09:10:30 +0100

    MyMGQS-ClassProject

---
+++ Chapter7-Array/Program.cs
@@ -0,0 +1,84 @@
+﻿class Program
+{
+    static void Array(string[] args)
+        {
+            int[] arr = new int[20];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i * 5;
+                Console.WriteLine(arr[i]);
+            }
+        }
+ static void AdvArray(string[] args)
+        {
+            bool arraysEqual = true;
+
+            Console.Write("Enter lenght of first array: ");
+            int length = Int32.Parse(Console.ReadLine()!);
+
+            int[] arrA = new int[length];
+
+            for (int i = 0; i < arrA.Length; i++)
+            {
+                Console.Write("Enter element {0}: ", i);
+                arrA[i] = Int32.Parse(Console.ReadLine()!);
+            }
+
+            Console.Write("\nEnter lenght of second array: ");
+
+            if (length != Int32.Parse(Console.ReadLine()!)) Console.WriteLine("\nArrays have different lengths.");
+            else
+            {
+                int[] arrB = new int[length];
+
+                for (int i = 0; i < arrB.Length; i++)
+                {
+                    Console.Write("Enter element {0}: ", i);
+                    arrB[i] = Int32.Parse(Console.ReadLine()!);
+                }
+
+                for (int i = 0; i < arrA.Length; i++)
+                {
+                    if (arrA[i] != arrB[i])
+                    {
+                        Console.WriteLine("\nArrays are different.");
+                        arraysEqual = false;
+                        break;
+                    }
+                }
+
+                if (arraysEqual) Console.WriteLine("\nArrays are the same.");
+            }
+        }
+static void Lexographical(string[] args)
+        {
+            bool arrayEqual = true;
+            char[] arrA = new char[5] { 'a', 'b', 'c', 'd', 'e' };
+            char[] arrB = new char[5] { 'a', 'b', 'c', 'd', 'e' };
+
+            if (arrA.Length > arrB.Length) Console.WriteLine("Second array is lexicographicaly first.");
+            else if (arrA.Length < arrB.Length) Console.WriteLine("First array is lexicographicaly first.");
+            else
+            {
+                for (int i = 0; i < arrA.Length; i++)
+                {
+                    if (arrA[i] < arrB[i])
+                    {
+                        Console.WriteLine("First array is lexicographicaly first.");
+                        arrayEqual = false;
+                        break;
+                    }
+                    if (arrA[i] > arrB[i])
+                    {
+                        Console.WriteLine("Second array is lexicographicaly first.");
+                        arrayEqual = false;
+                        break;
+                    }
+                }
+
+                if (arrayEqual) Console.WriteLine("Arrays are lexicographicaly equal.");
+            }
+        }  
+                      
+}
